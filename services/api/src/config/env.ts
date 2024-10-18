04ee0a619edbdac18cbbc7b55fff1fb4e7c86973
Ahmed Al Ghassani <ahmed-ghassani@outlook.com>
2024-10-18 18:53:52 +0400

    feat(api): add prisma and db config

--- services/api/src/config/env.ts
+++ services/api/src/config/env.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const envSchema = z.object({
   PROJECT_NAME: z.string(),
   APP_DOMAIN: z.string(),
+  API_DB_URL: z.string(),
 });
 
 const env = envSchema.safeParse(process.env);
@@ -15,4 +16,4 @@ if (!env.success) {
   process.exit(1);
 }
 
-export const { PROJECT_NAME, APP_DOMAIN } = env.data;
+export const { PROJECT_NAME, APP_DOMAIN, API_DB_URL } = env.data;
